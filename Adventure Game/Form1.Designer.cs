3389b6c093a682c56fc4fb772a70737e8b2b9585
Aaron Kim <aarokim318@ed.amdsb.ca>
2021-10-29 14:24:28 -0400

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option2Button = new System.Windows.Forms.PictureBox();
+            this.option1Button = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.option2Button)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.option1Button)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // option2Label
+            // 
+            this.option2Label.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.Location = new System.Drawing.Point(112, 398);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(574, 28);
+            this.option2Label.TabIndex = 14;
+            this.option2Label.Text = "option2Label";
+            // 
+            // option1Label
+            // 
+            this.option1Label.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.Location = new System.Drawing.Point(112, 342);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(578, 28);
+            this.option1Label.TabIndex = 13;
+            this.option1Label.Text = "option1Label";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(40, 18);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(654, 164);
+            this.outputLabel.TabIndex = 15;
+            this.outputLabel.Text = "outputLabel";
+            // 
+            // option2Button
+            // 
+            this.option2Button.Image = global::Adventure_Game.Properties.Resources.black_checkmark;
+            this.option2Button.Location = new System.Drawing.Point(26, 387);
+            this.option2Button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(66, 39);
+            this.option2Button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.option2Button.TabIndex = 12;
+            this.option2Button.TabStop = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option1Button
+            // 
+            this.option1Button.Image = global::Adventure_Game.Properties.Resources.black_checkmark;
+            this.option1Button.Location = new System.Drawing.Point(26, 330);
+            this.option1Button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(66, 40);
+            this.option1Button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.option1Button.TabIndex = 11;
+            this.option1Button.TabStop = false;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(737, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.option2Button)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.option1Button)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.PictureBox option2Button;
+        private System.Windows.Forms.PictureBox option1Button;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            outputLabel.Text = "you wake up 10 minutes before class starts, " +
+                "you quickly get ready and get out the door. Do you choose to run or ride the bike?";
+            option1Label.Text = "use the bike";
+            option2Label.Text = "run instead";
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+
+            if (page == 1) { page = 2;}
+            else if (page == 2) { page = 4;}
+            else if (page == 3) { page = 6;}
+            else if (page == 4) { page = 9;}
+            else if (page == 5) { page = 11;}
+            else if (page == 6) { page = 1;}
+            else if (page == 7) 
+            { 
+                Random randGen = new Random();
+                int randValue = randGen.Next(1, 11);
+                if( randValue < 7)
+                {
+                    page = 14;
+                }
+                else
+                {
+                    page = 13;
+                }
+            } 
+            else if (page == 8) { page = 1;}
+            else if (page == 9) { page = 97;}
+            else if (page == 10) { page = 98;}
+            else if (page == 11) { page = 1;}
+            else if (page == 12) { page = 16;}
+            else if (page == 13) { page = 1;}
+            else if (page == 14)
+            {
+                page = 19;
+            }
+            else if (page == 15) { page = 1;}
+            else if (page == 16) { page = 1;}
+            else if (page == 17) { page = 1;}
+            else if (page == 18) { page = 1;}
+            else if (page == 19) { page = 18;}
+            else if (page == 20) {  page = 1;}
+            else if (page == 21) {  page = 1;}
+            else if (page == 22) { page = 1;}
+ 
+            switch(page)
+            {
+                case 1:
+                    outputLabel.Text = "You wake up 10 minutes before class starts, " +
+                "you quickly get ready and get out the door. Do you choose to run or ride the bike?";
+                    option1Label.Text = "Use the bike.";
+                    option2Label.Text = "Run instead.";
+                    
+                    break;
+                case 2:
+                    outputLabel.Text = "You choose to run. As you are rapidly running to school," +
+                        " you see a lady that needs help crossing the road. " +
+                        "do you stop and help her or keep running to school?";
+                    option1Label.Text = "Stop and help her.";
+                    option2Label.Text = "Don't help her and keep running.";
+                    break;
+                case 3:
+                    outputLabel.Text = "You choose to bike. As you're biking, you see a train is quickly " +
+                        "coming. Do you keep biking to get across or stop and let the train go by?";
+                    option1Label.Text = "Keep biking and try to get across.";
+                    option2Label.Text = "Stop and wait until the train goes by.";
+                    break;
+                case 4:
+                    outputLabel.Text = "You decide to stop and help the lady cross the road." +
+                        " She rewards you with a turnip. You say your goodbyes and you rush off quickly." +
+                        " You see a shortcut in a alleyway, and you decidee to take it. As you are running " +
+                        "down the ally you see a biker gang making food. They seemed to be pissed off about" +
+                        "something, and it turns out they were making food but forgot to buy turnips." +
+                        "they get so pissed that they threaten you for you everything you have. do you give " +
+                        "them everything, try and run. Or give them the turnip you got from the lady?";
+                    option1Label.Text = "You give them everything";
+                    option2Label.Text = "You give them the turnip.";
+                    // option3Label.Text = "You try and run away. they catch you and beat you up so bad " +
+                    // that you get hospitalized.";
+                    break;
+                case 5:
+                    outputLabel.Text = "You decide to run past the lady. " +
+                        "As you are running towards school, you see a family of geese blocking your path." +
+                        "Do you try and jump over the family of geese? Or do you try and rush the gesse out of your way?";
+                    option1Label.Text = "try and rush the family of geese";
+                    option2Label.Text = "you jump over the family of geese";
+                    break;
+                case 6:
+                    outputLabel.Text = "you try and beat the train. the train is a speed-demon and you got hit and died. Play Again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "You paitiantly wait unitl the train has passed." +
+                        "You try and compensate for the wasted time waiting so you head down road that's downhill" +
+                        "As you're biking downhill, you see a pothole. Do you try and jump over the pothole?" +
+                        " Or do you stop and walk around the pothole? ";
+                    option1Label.Text = "you try and jump over the pothole";
+                    option2Label.Text = "you wait";
+                    break;
+                case 8:
+                    outputLabel.Text = "You stop and walk around the pothole, but you are too slow to school and this results in " +
+                        "detention for 2 weeks. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 9:
+                    outputLabel.Text = "You try and stand up for yourself. They beat you up and take your money and bike." +
+                        " You are late for class :(. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 10:
+                    outputLabel.Text = "You try and run away. but you trip on a rock while trying to escape. " +
+                        "They catch you and beat you up so bad that you get hospitalized. PLay again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 11:
+                    outputLabel.Text = "You rush the family of geese, but they end up getting annoyed and attack you." +
+                        "You get plucked to death. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 12:
+                    outputLabel.Text = "You jump over the family of geese with ease. You see the school in eyes view. " +
+                        "as you're about to reach the school you see a 10$ bill on the ground. Do you pick up the money or leave it.";
+                    option1Label.Text = "Happily pick up the money";
+                    option2Label.Text = "Leave the money but keep your pride";
+                    break;
+                case 13:
+                    outputLabel.Text = "You break your leg and your bike is total trying to" +
+                        " jump the pothole. You are in a cast for 1 year and you have to pay 1200$ to repair your bike," +
+                        " and the icing on the cake is that you are late for school with a week of detention. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 14:
+                    outputLabel.Text = "You are successful jumping the pothole whilge going downhill at 40 km/h. as you're biking," +
+                        "you see that you hit traffic. This blocks your route to school, luckily you see a forest that is somewhat familiar." +
+                        "Do you try your luck and go through the forest? Or try risking it all crossing the road?";
+                    option1Label.Text = "Go through the forest";
+                    option2Label.Text = "Risk it crossing the road";
+                    break;
+                case 15:
+                    outputLabel.Text = "You give them the turnip and they are so very happy. They reward you for giving them the turnip." +
+                        "They help you get to school with their motorcycles. You get to school with 5 minutes to spare! Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 16:
+                    outputLabel.Text = "You left the 10$ Bill and as you're running you come across the lady you didn't help at the light." +
+                        "It turns out that you bummed the her groceries onto the road and they got squished." +
+                        "You try and apologize to the lady but her sons deliquent gang comes and takes all your money  and beat you up. " +
+                        "You are late for class. Play Again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 17:
+                    outputLabel.Text = "you picked up the 10$ bill and as you're happily running to school you come across the lady from the light." +
+                        "It turns out that you knocked her phone from her hand as she is talking to her son. " +
+                        "You try and apologize to the lady but her sons deliquent gang comes and takes all your money  and beat you up. " +
+                        "You are late for clas. Play Again?"; 
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 18:
+                    outputLabel.Text = "As you go left, you trip and roll down a hill. 15 bears show up becaue they hear noise right outside their home." +
+                        "They are so hungry since they haven't eaten for 2 days. you get mauled and eaten. Play Again? ";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 19:
+                    outputLabel.Text = "As you go in the forest you see the path splits into two. Do you go Left or Right?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 20:
+                    outputLabel.Text = "You get hit by a car trying to cross the road. Play Again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 21:
+                    outputLabel.Text = "You somehow get through traffic without a scratch, and you get to school with a 4 minutes to spare. Play Again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 22:
+                    outputLabel.Text = "As you choose right, You see a white light at the end of the forest. You somehow get out!" +
+                        "You get to school with 2 minutes to spare. Play Again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thanks for Playing!";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    Thread.Sleep(2500);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+
+            }
+         
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1) { page = 3; }
+            else if (page == 2) { page = 5; }
+            else if (page == 3) { page = 7; }
+            else if (page == 4) { page = 15; }
+            else if (page == 5) { page = 12; }
+            else if (page == 6) { page = 99; }
+            else if (page == 7) { page = 8; }//NOT DONE 
+            else if (page == 8) { page = 99; }
+            else if (page == 9) { page = 99; }
+            else if (page == 10) { page = 99; }
+            else if (page == 11) { page = 99; }
+            else if (page == 12) { page = 16; }
+            else if (page == 13) { page = 99; }
+            else if (page == 14) { page = 19; }
+            else if (page == 15) { page = 99; }
+            else if (page == 16) { page = 99; }
+            else if (page == 17) { page = 99; }
+            else if (page == 18) { page = 99; }
+            else if (page == 19) { page = 18; }
+            else if (page == 20) { page = 99; }
+            else if (page == 21) { page = 99; }
+            else if (page == 22) { page = 99; }
+
+
+        }
+
+        
+    }
+}
